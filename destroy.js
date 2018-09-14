469109f63cd8cecb654fb23d2a9469be25bbfd6c
Che Huu <chequochuu@yahoo.com.vn>
2018-09-15 00:32:46 +0700

    fix bug

--- destroy.js
+++ destroy.js
@@ -3,13 +3,16 @@ async function clickPlanet() {
     var id = "21";
     var t = document.getElementsByClassName("dropdown")[0].options;
     var i;
+    var shoulddel = false;
     for (i= 0; i < t.length; i++){
         if (t[i].value == id) {
             t[i].selected =true;
+            shoulddel = true;
         }
     }
     var des = document.getElementsByClassName("button-content");
-    des[0].click();
-    
+    if (shoulddel){
+        des[0].click();
+    }
 }
 clickPlanet();
